267659d66a50f8ab08e8368c7f3ab7f1e32dbea3
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-10-02 16:06:50 -0700

    Setup simple server with express and handlebars

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  handlebars({
+    extname: "hbs",
+    defaultLayout: "index",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("main", { scriptName: "index.ts" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
