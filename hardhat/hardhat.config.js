db1e2de9aca355a64f0cfa8f289018155ea4d684
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-10-03 15:12:09 -0700

    Fork art blocks contracts

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
-import "@nomiclabs/hardhat-waffle"
-import "@nomiclabs/hardhat-etherscan"
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.5.0",
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
   }
