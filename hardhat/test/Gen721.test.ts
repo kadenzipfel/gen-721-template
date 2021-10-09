0a855e5d5762a2bbab5c7b920cda6647e8eec185
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-10-09 12:49:12 -0700

    test: it sets token name and symbol

--- hardhat/test/Gen721.test.ts
+++ hardhat/test/Gen721.test.ts
@@ -1,11 +1,27 @@
+import { parseUnits } from "@ethersproject/units";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Gen721", () => {
+  const TOKEN_NAME = "Gen721";
+  const TOKEN_SYMBOL = "GEN";
+  let owner, user, Gen721;
   beforeEach(async () => {
-    const [owner, user] = await ethers.getSigners();
+    [owner, user] = await ethers.getSigners();
+    const Gen721Factory = await ethers.getContractFactory("Gen721");
+    Gen721 = await Gen721Factory.deploy(
+      TOKEN_NAME,
+      TOKEN_SYMBOL,
+      "gateway.ipfs.io/ipfs/example/",
+      parseUnits("0.05", "ether"),
+      5000
+    );
   });
-  it("works", () => {
-    console.log("yup");
+
+  it("sets token name and symbol", async () => {
+    const tokenName = await Gen721.name();
+    const tokenSymbol = await Gen721.symbol();
+    expect(tokenName).to.equal(TOKEN_NAME);
+    expect(tokenSymbol).to.equal(TOKEN_SYMBOL);
   });
 });
