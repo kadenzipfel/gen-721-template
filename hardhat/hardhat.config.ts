2d8551655ff69365db058f81424e178f669eff55
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-10-10 14:33:42 -0700

    feat: add rinkeby to config

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -6,6 +6,12 @@ import "@nomiclabs/hardhat-etherscan";
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIV_KEY],
+    },
+  },
   solidity: "0.5.0",
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
