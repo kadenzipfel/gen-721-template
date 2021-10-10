acd0eff7e6d17efc9aeeb2fd7c046ddfa0ca7ddf
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-10-10 13:42:15 -0700

    feat: return metadata from /:tokenId

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const url = require("url");
 
 const app = express();
 const port = 3000;
+
+function fullUrl(req, path) {
+  return url.format({
+    protocol: req.protocol,
+    host: req.get("host"),
+    pathname: path,
+  });
+}
 
 app.set("view engine", "hbs");
 app.engine(
@@ -15,6 +24,17 @@ app.engine(
 );
 
 app.use(express.static("public"));
+
+app.get("/:tokenId", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(
+    JSON.stringify({
+      name: `#${req.params.tokenId}`,
+      description: "A series of generative art NFTs.",
+      image: fullUrl(req, `/image/${req.params.tokenId}`),
+    })
+  );
+});
 
 app.get("/", (req, res) => {
   res.render("main", { scriptName: "index.js" });
